915f21327fee6689ced2404ee6921f3c352ae361
Dr. Saylermorph <drsdrs@users.noreply.github.com>
2017-01-10 16:19:53 +0100

    Update turtleInterface.lua

--- turtleInterface.lua
+++ turtleInterface.lua
@@ -1,46 +1,95 @@
 print("Load turtleApi: ", os.loadAPI("turtleApi"))
+local T = turtleApi
 
 local menuItems = {
-	"bla", "bleee",
-	"2bla", "3bleee",
-	"2bla", "3bleee"
+	"setPos: x,y,z        	   |     ",
+	"setRotation: rotation	   |     ",
+	"goto: x,y,z,force    	   |     ",
+	"digFwd3x3: length        |     ",
+	"digDwn: size,depth       |     ",
 }
 
-local menuLength = 6
+local menuLength = 5
 
 local width,height = term.getSize()
 
 function showPosRot()
-	term.setTextColor(colors.yellow)
 	term.setCursorPos(width-5, 1)
-	term.write("X:"..turtleApi.position[0].."  ")
+	term.write("X:"..T.position[0].."   ")
 	term.setCursorPos(width-5, 2)
-	term.write("Y:"..turtleApi.position[1].."  ")
+	term.write("Y:"..T.position[1].."   ")
 	term.setCursorPos(width-5, 3)
-	term.write("Z:"..turtleApi.position[2].."  ")
+	term.write("Z:"..T.position[2].."   ")
+	term.setCursorPos(width-5, 4)
+	term.write("R:"..T.getRotation().."   ")
 end
 
+function showMenuItems()
+	for row=0, menuLength do
+		term.setCursorPos(1,row)
+		term.write(row.."="..tostring(menuItems[row]))
+	end
+	term.setCursorPos(1,menuLength+1)
+	term.write("---------------------------------------------")
 
+end
 
-for x=1, width do
-	for y=1, height do
-		paintutils.drawPixel(x,y,colors.green)
+function waitForCommand()
+	local command = read()
+	local splited = split(command, " ")
+	if tablelength(splited)<2 then return print("Input error") end
+	local args = split(splited[2], ",")
+	if tablelength(args)<1 then return print("Input error") end
+	local funct = tonumber(splited[1])
+	if args[4]=="true" then args[4]=true else args[4]=false end
+	excecuteCommand(funct,tonumber(args[1]),tonumber(args[2]),tonumber(args[3]),args[4])
+end
+
+function show(text)
+	term.setCursorPos(1, height)
+	term.write(text)
+end
+
+function excecuteCommand(cmd, arg0, arg1, arg2, arg3)
+	if cmd==1 then T.setPos(arg0, arg1, arg2)
+	elseif cmd==2 then T.setRotation(arg0)
+	elseif cmd==3 then T.goto(arg0, arg1, arg2, arg3)
+	elseif cmd==4 then T.dig3x3(arg0)
+	elseif cmd==5 then T.digDown(arg0, arg1)
 	end
 end
 
-term.setBackgroundColor(colors.green)
-term.setTextColor(colors.black)
-
-
-for row=0, menuLength do
-	term.setCursorPos(1,row)
-	term.write(row..": "..tostring(menuItems[row]))
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
 end
 
-showPosRot()
+function split(str, pat)
+   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+	 table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
+
+term.clear()
 
 while true do
-  local evt, key = os.pullEvent("key")
-	term.setCursorPos(1,height-1)
-	term.write(": "..(key-1).."   ")
+	showMenuItems()
+	showPosRot()
+	term.setCursorPos(1,height)
+	waitForCommand()
+
 end
